ee8f86c970c97c9648674d5f8e6ce5d7fedc9e52
Alex <14wright08@gmail.com>
2024-08-06 13:01:49 +0100

    Test home and test gripper

---
+++ examples/test_gripper.py
@@ -0,0 +1,17 @@
+
+from robotiq.robotiq_gripper import RobotiqGripper
+
+HOST = "192.168.0.2"
+PORT = 30003
+
+def main():
+    #tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #tcp_socket.connect((HOST, PORT))
+    gripper=RobotiqGripper()
+    gripper.connect(HOST, 63352)
+    gripper.activate()
+    gripper.move(0,1,2)
+
+
+if __name__ == '__main__':
+    main()
